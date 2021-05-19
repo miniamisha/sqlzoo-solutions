ac054362f8895a8195e992428b630b303ace1768
Amisha Rawat <amishaece10@gmail.com>
2021-05-19 07:39:59 -0700

    Update SELECT_from_WORLD_Tutorial.sql

--- SELECT_from_WORLD_Tutorial.sql
+++ SELECT_from_WORLD_Tutorial.sql
@@ -1,1 +1,25 @@
+Q1. 
+Observe the result of running this SQL command to show the name, continent and population of all countries.
+
+SELECT name, continent, population FROM world
+
+
+Q2.
+How to use WHERE to filter records. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+
+SELECT name
+  FROM world
+ WHERE population > 2000000000
+ 
+ 
+ Q3.
+ Give the name and the per capita GDP for those countries with a population of at least 200 million.
+ 
+ select name , gdp/1000000000000  
+from world
+where population>200000000
+ 
+
+
+
 
