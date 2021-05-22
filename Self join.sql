ddad86386877c9b11fed5b052735d8de0fb0134a
Amisha Rawat <amishaece10@gmail.com>
2021-05-22 00:43:39 -0700

    Update Self join.sql

--- Self join.sql
+++ Self join.sql
@@ -86,20 +86,7 @@ WHERE S1.name='Craiglockhart'
   
  Q10.
  Find the routes involving two buses that can go from Craiglockhart to Lochend.
- 
- SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
-FROM route a JOIN route b
-ON (a.company = b.company AND a.num = b.num)
-JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
-JOIN stops stopa ON (a.stop = stopa.id)
-JOIN stops stopb ON (b.stop = stopb.id)
-JOIN stops stopc ON (c.stop = stopc.id)
-JOIN stops stopd ON (d.stop = stopd.id)
-WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Sighthill'
-            AND  stopb.name = stopc.name
-ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num
-;
- 
+
 
 
 
